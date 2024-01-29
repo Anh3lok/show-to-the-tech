b15f195112fb1e564f8fd0eb506139adf460078f
Anh3lok <anh310k@gmail.com>
2024-01-30 01:27:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace nekit
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 0; 
+        // устанавливаем метод обратного вызова
+        TimerCallback tm = new TimerCallback(Count);
+        // создаем таймер
+        Timer timer = new Timer(tm, num, 0, 1000);
+        Console.ReadLine();
+    }
+    public static void Count(object obj)
+    {
+    
+            for (int x= 0; x < 1; x++)
+        {
+            Console.WriteLine("саламуалейкум" );      
+        }
+    }
+}
+}
